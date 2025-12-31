cb32752285e9892ff8486ba3bf5bd9ad599859db
rachit069 <rachit694.be22@chitkara.edu.in>
2025-12-31 11:46:16 +0530

    Ques2

---
+++ 31December2025/src/Ques2.java
@@ -0,0 +1,11 @@
+/*Sam’s mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100. Find the average percent mark in PCM*/
+public class Ques2 {
+    public static void main(String args[]){
+        int math = 94;
+        int phy = 95;
+        int chem = 96;
+        int average = (math+phy+chem)/3;
+
+        System.out.println("Sam’s average mark in PCM is "+  average);
+    }
+}
