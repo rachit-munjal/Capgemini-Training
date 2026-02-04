3944e76aacc11118ccdb12d3394aebf641c5aa5e
rachit069 <rachit694.be22@chitkara.edu.in>
2026-02-04 22:56:48 +0530

    04-02-26/practice

---
+++ Practice/src/ReverseLL.java
@@ -0,0 +1,15 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class ReverseLL {
+    public static void main(String[] args){
+
+    }
+}
+
